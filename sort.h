bf69c36029f19c2058bf96efdb573f6a2aeba139
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2024-01-23 23:00:43 -0800

    Insertion sort

--- sort.h
+++ sort.h
@@ -3,7 +3,19 @@
 
 #include <stddef.h>
 
-/* Function prototypes*/
+/* Doubly linked list node structure */
+typedef struct listint_s {
+    const int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
 void bubble_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+listint_t *create_listint(const int *array, size_t size);
+void free_listint(listint_t *list);
+void insertion_sort_list(listint_t **list);
 
 #endif /* SORT_H */
+
