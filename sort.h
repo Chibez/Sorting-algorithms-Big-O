2ef006f174d58fb9d0bc4c2a5ece4cb5a2eaf4b4
Caleb Odobi Chibueze <odobicaleb@gmail.com>
2024-01-23 22:47:40 -0800

    Bubble sort

--- sort.h
+++ sort.h
@@ -3,8 +3,7 @@
 
 #include <stddef.h>
 
-/* Function prototypes */
+/* Function prototypes*/
 void bubble_sort(int *array, size_t size);
-void print_array(const int *array, size_t size);
 
 #endif /* SORT_H */
